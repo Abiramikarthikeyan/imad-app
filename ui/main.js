ab66e7c9a43d4dfb7c5ccfa9219ff768024bfb67
Abiramikarthikeyan <abiramikca@gmail.com>
2017-08-28 21:35:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element=document.getElementById('main-text'
 element.innerHTML='New value';
 //move the image
 var img=document.getElementById('http://www.hdwallpapersfreedownload.com/uploads/large/cartoons/tom-and-jerry-best-friends-free-hd-wallpaper.jpg');
-img.onclick=function(){
+img.onClick=function(){
     img.style.marginLeft='100px';
     
 };
