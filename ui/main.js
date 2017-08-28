47e7be51fdfdbc3069706b21c64a3dbc6b5cd8b1
Abiramikarthikeyan <abiramikca@gmail.com>
2017-08-28 21:45:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element=document.getElementById('main-text'
 element.innerHTML='New value';
 //move the image
 var img=document.getElementById('madi');
-img.onClick=function ()
+img.onclick=function ()
 {
     img.style.marginLeft='100px';
     
